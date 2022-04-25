6d8caa0ea6a9214b57d543b7eac1d6b421435d4b
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-04-25 22:28:05 +0530

    Add files via upload

--- Day-8 - Caesar Cipher encode/main.py
+++ Day-8 - Caesar Cipher encode/main.py
@@ -1,1 +1,31 @@
+# .....................Caesar Cipher (Encode and Decode)...................... #
+# ...............Created and modified by N.S.Bhanuprakash on 11-03-2022 ............. #
+from art import logo
+print(logo)
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    elif char not in alphabet:
+      end_text += char
+    
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+should_restart = True
+while should_restart is True:
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  shift = shift%26  
+  caesar(start_text=text, shift_amount=shift, 
+        cipher_direction=direction)
+  restart = input("Do you want to re-start the program - yes or no?").lower()
+  if restart == "no":
+    should_restart = False
+    print("good bye")
