c23e54b970cc1411f8ad45493466b6fb94514f0e
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-05-03 20:58:52 +0530

    Add files via upload

--- Day-17 - Quiz Game/main.py
+++ Day-17 - Quiz Game/main.py
@@ -1,1 +1,22 @@
+# ................................. Quiz Game ................................. #
+# ..........Created and modified by N.S.Bhanuprakash on 24-03-2022 .............#
 
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    question_object = Question(question_text, question_answer)
+    question_bank.append(question_object)
+# print(question_bank[0].question_text)
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
+    # quiz.check_answer(user_answer=user_answer)
+print("You have completed the quiz.")
+print(f"Your final score is {quiz.score}/{quiz.question_number}")
+# print(bank1[0].question_text)
+# print(question_data[0]["text"])
