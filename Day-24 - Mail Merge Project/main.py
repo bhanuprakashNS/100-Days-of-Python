acd01999186f96cdf9436c7a7d66699eca988a00
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-05-15 23:06:05 +0530

    Add files via upload

--- Day-24 - Mail Merge Project/main.py
+++ Day-24 - Mail Merge Project/main.py
@@ -1,1 +1,24 @@
+# ................... Mail Merge Project .................................. #
+# ............. Created and modified by N.S.Bhanuprakash on 01-04-2022 .....#
 
+REPLACEMENT_TEXT = "[name]"
+
+with open("./Input/Names/invited_names.txt") as invitee:
+    names = invitee.readlines()
+for name in names:
+    name = name.strip()
+    with open(f"./Output/ReadyToSend/{name}.txt", "a") as new_file:
+        letter = open("./Input/Letters/starting_letter.txt", "r")
+        lines = letter.read()
+        new_file.write(lines.replace(REPLACEMENT_TEXT, name))
+        letter.close()
+
+        # Another method using "readlines" command ..............
+        # lines = letter.readlines()
+        # first_line = lines[0]
+        # new_first_line = first_line.replace(REPLACEMENT_TEXT, name)
+        # lines[0] = new_first_line
+        # num = 0
+        # for num in range(len(lines)):
+        #     new_file.write(lines[num])
+        # letter.close()
