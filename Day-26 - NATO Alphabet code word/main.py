feddbb10d5f192a8e5876cefed5ad8fb5aa6f510
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-05-18 22:39:14 +0530

    Add files via upload

--- Day-26 - NATO Alphabet code word/main.py
+++ Day-26 - NATO Alphabet code word/main.py
@@ -1,1 +1,32 @@
+# ................ NATO Alphabet code word ................................ #
+# ............. Created and modified by N.S.Bhanuprakash on 04-04-2022 .....#
+import pandas
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+
+nato_data = pandas.read_csv("nato_phonetic_alphabet.csv")
+NATO_alphabet_data = {data.letter: data.code for (index, data) in nato_data.iterrows()}
+# print(NATO_alphabet_data)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+
+
+def create_code_word():
+    user_input = input("What's your word?\n").upper()
+    try:
+        user_code = [NATO_alphabet_data[letter] for letter in user_input]
+        print(user_code)
+    except KeyError:
+        print("Please enter only NATO alphabets")
+        create_code_word()
+
+
+create_code_word()
+
+
+
+
 
