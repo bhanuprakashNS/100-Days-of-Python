a83d64cd03d67a9e753592cd083353fafc93a708
Bhanu222 <103272212+bhanuprakashNS@users.noreply.github.com>
2022-07-05 22:41:53 +0530

    Add files via upload

--- Day-56 - Name Card Project/main.py
+++ Day-56 - Name Card Project/main.py
@@ -1,1 +1,15 @@
+# .............................. Name Card ................................ #
+# ............. Created and modified by N.S.Bhanuprakash on 16-05-2022 .....#
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def name_card():
+    return render_template("/index.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
